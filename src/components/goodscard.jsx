d06a9321a9306a56378686eaf933619829be3559
15689670104@163.com <02048073@yto.net.cn>
2021-03-02 17:51:11 +0800

    删除linaria 依赖

---
+++ src/components/goodscard.jsx
@@ -0,0 +1,48 @@
+// eslint-disable-next-line no-unused-vars
+import React ,{Component} from 'react'
+import { AtGrid } from "taro-ui"
+import "taro-ui/dist/style/components/grid.scss";
+
+export default class Goodscard extends Component {
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      goodsData: [
+        {
+          image: 'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png',
+          value: '领取中心'
+        },
+        {
+          image: 'https://img20.360buyimg.com/jdphoto/s72x72_jfs/t15151/308/1012305375/2300/536ee6ef/5a411466N040a074b.png',
+          value: '找折扣'
+        },
+        {
+          image: 'https://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png',
+          value: '领会员'
+        },
+        {
+          image: 'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png',
+          value: '新品首发'
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <AtGrid columnNum={4} hasBorder={false} data={this.state.goodsData} />
+    )
+  }
+}
+
+
